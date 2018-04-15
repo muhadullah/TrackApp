ae9fe18b910eb30972ab4ad8a5be46f18607f9b7
YoDaddysSon <Mabdullah@wccnet.edu>
2018-04-15 04:18:00 -0400

    --

---
+++ app/src/main/java/com/my/newproject/LoginActivity2.java
@@ -0,0 +1,15 @@
+package com.my.newproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+import java.util.*;
+import android.util.*;
+
+public class LoginActivity2 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+    }
+}
